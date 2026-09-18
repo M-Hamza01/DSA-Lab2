96557b9caf59edaeaebd4319afb068fb838f8e88
M Hamza <mhamza9526322@gmail.com>
2026-09-18 16:06:02 +0500

    Implement isSorted function

--- isSorted.cpp
+++ isSorted.cpp
@@ -3,7 +3,14 @@
 using namespace std;
 
 bool isSorted(const int *arr, const int size) {
-    return false; 
+    if (size <= 1) return true; 
+    // Compare neighboring elements
+     for (int i = 0; i < size - 1; i++) {
+        if (arr[i] > arr[i+1]) { 
+            return false;
+        }
+    }
+    return true;
 }
 
 void testSortedArray() {
@@ -15,10 +22,46 @@ void testUnsortedArray() {
     int arr[] = {1, 4, 3, 5, 6};
     assert(isSorted(arr, 5) == false); 
 }
+void testDuplicateValues() {
+    int arr[] = {1, 2, 2, 3, 5};
+    assert(isSorted(arr, 5) == true);
+}
+
+void testSingleElement() {
+    int arr[] = {42};
+    assert(isSorted(arr, 1) == true);
+}
+
+void testDescendingArray() {
+    int arr[] = {5, 4, 3, 2, 1};
+    assert(isSorted(arr, 5) == false);
+}
+
+void testNegativeValues() {
+    int arr[] = {-10, -5, 0, 2};
+    assert(isSorted(arr, 4) == true);
+}
+
+// Two additional custom test cases
+void testAllSameElements() {
+    int arr[] = {7, 7, 7, 7};
+    assert(isSorted(arr, 4) == true);
+}
+
+void testEmptyArray() {
+    int arr[4] = {};
+    assert(isSorted(arr, 0) == true);
+}
 
 int main() {
-    testSortedArray();
+   testSortedArray();
     testUnsortedArray();
+    testDuplicateValues();
+    testSingleElement();
+    testDescendingArray();
+    testNegativeValues();
+    testAllSameElements();
+    testEmptyArray();
     cout << "All TDD tests passed successfully!\n";
     return 0;
 }
